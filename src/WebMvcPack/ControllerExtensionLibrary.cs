9eebe2fb9224ae9e9da4de4a9c8c9f4044640619
CreatioVitae <h.chiba201103@gmail.com>
2021-01-18 23:30:25 +0900

    first commit...(repair) (#1)

---
+++ src/WebMvcPack/ControllerExtensionLibrary.cs
@@ -0,0 +1,64 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+
+namespace Microsoft.AspNetCore.Mvc {
+    public static class ControllerExtensionLibrary {
+        [NonAction]
+        public static ViewResult BadRequestView(this Controller controller, (string key, string errorMessage)? modelError) =>
+            controller.AddModelErrorIfModelErrorHasValue(modelError).CreateViewWithHttpStatusCode(null, HttpStatusCode.BadRequest);
+
+        [NonAction]
+        public static ViewResult BadRequestView(this Controller controller, string viewName, object model, (string key, string errorMessage)? modelError) =>
+            controller
+                .AddModelErrorIfModelErrorHasValue(modelError)
+                .AddModelInViewDataIfModelHasValue(model)
+                .CreateViewWithHttpStatusCode(viewName, HttpStatusCode.BadRequest);
+
+        [NonAction]
+        public static ViewResult BadRequestView(this Controller controller, object model, (string key, string errorMessage)? modelError) =>
+            controller
+                .AddModelErrorIfModelErrorHasValue(modelError)
+                .AddModelInViewDataIfModelHasValue(model).
+                CreateViewWithHttpStatusCode(null, HttpStatusCode.BadRequest);
+
+        [NonAction]
+        public static ViewResult BadRequestView(this Controller controller, string viewName, (string key, string errorMessage)? modelError) =>
+            controller
+                .AddModelErrorIfModelErrorHasValue(modelError)
+                .CreateViewWithHttpStatusCode(viewName, HttpStatusCode.BadRequest);
+
+        [NonAction]
+        public static ViewResult CreateHttpGetViewResult(this Controller controller, object? model, (string key, string errorMessage)? modelError = default) =>
+             model switch {
+                 IEnumerable<object> e when e.Any() => controller.AddModelErrorIfModelErrorHasValue(modelError).View(e),
+                 IEnumerable<object> e when !e.Any() => controller.AddModelErrorIfModelErrorHasValue(modelError).CreateViewWithHttpStatusCode(null, HttpStatusCode.NotFound),
+                 object => controller.AddModelErrorIfModelErrorHasValue(modelError).View(model),
+                 null => controller.AddModelErrorIfModelErrorHasValue(modelError).CreateViewWithHttpStatusCode(null, HttpStatusCode.NotFound)
+             };
+
+        internal static Controller AddModelErrorIfModelErrorHasValue(this Controller controller, (string key, string errorMessage)? modelError) {
+            if (modelError is not null) {
+                controller.ModelState.AddModelError(modelError.Value.key, modelError.Value.errorMessage);
+            }
+
+            return controller;
+        }
+
+        internal static Controller AddModelInViewDataIfModelHasValue(this Controller controller, object model) {
+            if (model is not null) {
+                controller.ViewData.Model = model;
+            }
+
+            return controller;
+        }
+
+        internal static ViewResult CreateViewWithHttpStatusCode(this Controller controller, string? viewName, HttpStatusCode statusCode) =>
+            new ViewResult {
+                ViewName = viewName,
+                ViewData = controller.ViewData,
+                TempData = controller.TempData,
+                StatusCode = (int)statusCode
+            };
+    }
+}
