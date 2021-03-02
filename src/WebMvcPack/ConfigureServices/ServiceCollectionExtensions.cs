4723c0d7089b2e4d01c975d5819a6a8ff3c2971b
CreatioVitae <h.chiba201103@gmail.com>
2021-03-02 14:09:28 +0900

    add redis session... (#2)

---
+++ src/WebMvcPack/ConfigureServices/ServiceCollectionExtensions.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.DataProtection;
+using StackExchange.Redis;
+
+namespace Microsoft.Extensions.DependencyInjection {
+    public static class ServiceCollectionExtensions {
+        public static IServiceCollection AddRedisSession(this IServiceCollection services, RedisSessionOptions redisSessionOptions) {
+            services
+                .AddDataProtection()
+                .SetApplicationName(redisSessionOptions.ApplicationName)
+                .PersistKeysToStackExchangeRedis(ConnectionMultiplexer.Connect(redisSessionOptions.ConnectionString), redisSessionOptions.RedisKey);
+
+            services.AddSession(options => {
+                options = redisSessionOptions.SessionOptions;
+            });
+
+            return services;
+        }
+    }
+}
