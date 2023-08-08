d05c3a65c2d71b1cb7835ab4c84432840a596c76
hchiba1984 <h.chiba201103@gmail.com>
2023-08-08 23:19:48 +0900

    upgrade package. (#3)

--- src/WebMvcPack/ConfigureServices/RedisSessionOptions.cs
+++ src/WebMvcPack/ConfigureServices/RedisSessionOptions.cs
@@ -1,4 +1,3 @@
 namespace Microsoft.AspNetCore.Builder;
 
-public record RedisSessionOptions(string ApplicationName, string ConnectionString, string RedisKey, SessionOptions SessionOptions) {
-}
+public record RedisSessionOptions(string ApplicationName, string ConnectionString, string RedisKey, SessionOptions SessionOptions);
